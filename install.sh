683ed8251e260bb5b3e08b703961d384f1b96129
FikiXyz <131833746+FikiXyz@users.noreply.github.com>
2023-05-07 08:07:23 +0700

    Update install.sh
    P

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ installTheme(){
     echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r Utama
-    git clone https://github.com/ZassTdr/Utama.git
+    git clone https://github.com/FikiXyz/Utama.git
     cd Utama
     rm /var/www/pterodactyl/resources/scripts/Utama.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
@@ -47,7 +47,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/ZassTdr/Utama/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/FikiXyz/Utama/main/repair.sh)
 }
 
 restoreBackUp(){
